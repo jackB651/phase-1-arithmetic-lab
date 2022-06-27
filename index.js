a3ea11cf619535f4ed2f290b93505459d7534245
Jonathon <jwbialas@gmail.com>
2022-06-27 15:35:05 -0500

    commit tree dbe6b325edf4d92fa1b3f25988c7f75459e00313

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1;
+let num2;
+function multiply (num1, num2){
+    return num1*num2;
+}
+function random (){
+    return Math.random();
+}
+let num3;
+function mod (num3, num4){
+    return ;
+}
+function max (num5){
+    return Math.max();
+}
